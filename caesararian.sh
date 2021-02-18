cef1789b31df7935f6564ed971695321993b5207
Bradley Stradling <45749708+Bradley-Stradling@users.noreply.github.com>
2021-02-17 21:24:04 -0500

    Update caesararian.sh
    This script does not handle spaces or scpecial char's in the input file, and does not output spcae's but is functional

--- caesararian.sh
+++ caesararian.sh
@@ -5,8 +5,8 @@
 #  (o.o)	Date of first revision: Fri 12 Feb 2021 01:33:14 PM EST
 # (")_(")	Lincense: https://unlicense.org/
 #*******************************************************************************
-# (\_/)		Simple Caesar cipher script.
-# (*.*)		
+# (\_/)		Simple Caesar cipher script. That can handle encipher or 
+# (*.*)		decipher mode, and any rotation count between 0 and 26
 #(")_(")	
 #*******************************************************************************
 
@@ -20,7 +20,17 @@ black=`tput setaf 0`
 reset=`tput sgr0`
 
 show_Help() {
-echo -e "Example run \"./caesar.sh -r 3 -i infile -o outfile\""
+echo -e "Example run ${red}encipher${reset} by rotation value of 3 \n \"./caesar.sh -e -r 3 -i infile -o outfile\""
+echo -e "or \n \"./caesar.sh --encipher -rotation 3 -input_File infile -output_File outfile\"\n"
+echo -e "Example run ${red}decipher${reset} by rotation value of 3 \n \"./caesar.sh -d -r 3 -i infile -o outfile\""
+echo -e "or \n \"./caesar.sh --decipher -rotation 3 -input_File infile -output_File outfile\""
+
+echo ""
+
+echo -e "The 1st option passed should be\n -e or --encipher # to flag encipher or dicpher"
+echo -e "The 2nd option passed should be\n -e or --rotation # to flag the rotation count followed by a number between 0 and 26"
+echo -e "The 3rd option passed should be\n -i or --input_File # to flag the input file followed by the path/name of the file"
+echo -e "The 4th option passed should be\n -o or --output_FIle # to flag the output file followed by the path/name of the file"
 }
 
 #check_Help
@@ -55,28 +65,49 @@ echo "Missing"
 echo "${yellow}Please pass all needed options and parameters. Use -h to see help. Exiting${reset}"
 exit 1
 fi
+if [[ -z "$7" ]]; then
+echo "Missing"
+echo "${yellow}Please pass all needed options and parameters. Use -h to see help. Exiting${reset}"
+exit 1
+fi
 
 #set_Arguments
-if [[ $1 == "-r" || $1 == --rotation ]]; then
-rotation=$2
+if [[ $1 == "-e" || $1 == --encipher ]]; then
+cipher="encipher"
+elif [[ $1 == "-d" || $1 == --decipher ]]; then
+cipher="decipher"
+else
+echo "${yellow}Please pass options in correct order. Use -h to see help. Exiting${reset}"
+exit
+fi
+if [[ $2 == "-r" || $2 == --rotation ]]; then
+if [[ $cipher == "encipher" ]]; then
+rotation=$3
+elif [[ $cipher == decipher ]]; then
+rotation=$((26 - $3))
 elif [[ $1 != "-r" ]]; then
 echo "${yellow}Please pass options in correct order. Use -h to see help. Exiting${reset}"
 exit 1
 fi
-if [[ $3 == "-i" || $3 == --input_File ]]; then
-input_File=$4
-elif [[ $3 != "-i" ]]; then
+fi
+if [[ $4 == "-i" || $4 == --input_File ]]; then
+input_File=$5
+elif [[ $4 != "-i" ]]; then
 echo "${yellow}Please pass options in correct order. Use -h to see help. Exiting${reset}"
 exit 1
 fi
-if [[ $5 == "-o" || $5 == --output_File ]]; then
-output_File=$6
-elif [[ $5 != "-o" ]]; then
+if [[ $6 == "-o" || $6 == --output_File ]]; then
+output_File=$7
+elif [[ $6 != "-o" ]]; then
 echo "${yellow}Please pass options in correct order. Use -h to see help. Exiting${reset}"
 exit 1
 fi
 
 #check_Choices
+if [[ -z "$cipher" ]]; then
+echo "${red} cipher variable was not set :( Exiting${reset}"
+exit 1
+fi
 if [[ -z "$rotation" ]]; then
 echo "${red} rotation variable was not set :( Exiting${reset}"
 exit 1
@@ -90,8 +121,14 @@ echo "${red} output_File variable was not set :( Exiting${reset}"
 exit 1
 fi
 
+if [[ $cipher == "encipher" ]]; then
+echo "${green}Encipher mode set${reset}"
+elif [[ $cipher == "decipher" ]]; then
+echo "${green}Decipher mode set${reset}"
+fi
+
 #check_File_Locations
-if [ $rotation -lt 26 ] && [ $rotation -gt -26 ]; then
+if [ $rotation -lt 26 ] && [ $rotation -gt 0 ]; then
 echo -e "${green}Rotation set to $rotation ${reset}"
 else
 echo "${red}Unable to set the rotation to $rotation :( Exiting${reset}" && exit 1
@@ -125,25 +162,39 @@ echo ${yellow}${lowercase[@]}
 shifted_Lowercase=("${lowercase[@]:$rotation}" "${lowercase[@]:0:$rotation}")
 echo ${shifted_Lowercase[@]}${purple}
 
+#Encipher
 while read -n1 char; do
 count=0
 for input in ${uppercase[@]}
 do
+if [[ $char == " " ]]; then
+echo "a space was detected"
+fi
 if [[ $char == $input ]]; then
 upper_Or_Lower="upper"
 position=$(($count - $rotation)) # need to handle this being a negative number
+if [[ $position -lt "0" ]]; then
+position=$(($position + 26))
+fi
 echo "${green}$char${purple} is uppercase at position $count and rotates by $rotation back to $position as ${green}${uppercase[$position]}"
+echo -n ${uppercase[$position]} >> $output_File
 fi
 count=$(($count + 1))
 done
+#echo "${uppercase[$position]}"
 count=0
 for input in ${lowercase[@]}
 do
 if [[ $char == $input ]]; then
 upper_Or_Lower="lower"
 position=$(($count - $rotation)) # need to handle this being a negative number
+if [[ $position -lt "0" ]]; then
+position=$(($position + 26))
+fi
 echo "${green}$char${purple} is lowercase at position $count and rotates by $rotation back to $position as ${green}${lowercase[$position]}"
+echo -n ${lowercase[$position]} >> $output_File
 fi
 count=$(($count + 1))
 done
 done < $input_File
+
